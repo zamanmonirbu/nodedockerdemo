5e5cfe809d0051f67817ab81c7469dd484469211
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2026-02-11 22:14:42 +0600

    extend new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Node.js + Docker! 🚀</h1><p>আজকের তারিখ:: ' + new Date().toLocaleString() + '</p>');
+  res.send('<h1>Hello from Node.js + Docker! 🚀</h1><p>আজকের তারিখ::: ' + new Date().toLocaleString() + '</p>');
 });
 
 app.listen(port, () => {
